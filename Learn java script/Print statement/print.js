75f36907da1e05d135a47cd698f7ba97139454f2
pradeeb <valanpradeep6@gmail.com>
2025-03-27 19:07:22 +0530

    print statment

---
+++ Learn java script/Print statement/print.js
@@ -0,0 +1,14 @@
+
+console.info("This is an info message");    // Informational message
+console.warn("This is a warning message");  // Warning message
+console.error("This is an error message");  // Error message
+
+document.write("Hello, World!");  // Directly writes to the page
+
+//alert("Hello, World!");  
+//confirm("test");
+
+console.clear();
+
+let user=prompt("enter your age");
+console.error(user);  // Error message
