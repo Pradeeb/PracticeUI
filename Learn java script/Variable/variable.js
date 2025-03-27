9b43035cb4ff4342fa19724927cf767100aff86f
pradeeb <valanpradeep6@gmail.com>
2025-03-27 19:07:05 +0530

    varibale

---
+++ Learn java script/Variable/variable.js
@@ -0,0 +1,36 @@
+// *******************   var  **********************
+
+var age =30;  //Declaratuion and initialization
+
+var age;      //Declaratuion
+
+age=40;      //initialization and assigning
+
+var age=70;  //re-declaration 
+
+age="valan"  //re-assign 
+
+//console.log(age);
+
+
+// *******************   let  **********************
+
+let num=150; // Decleration and Initilazition
+
+num=10;      // initialization and assigning
+
+//let num;     // Cannot redeclare block-scoped variable 'num'.
+
+num="valan"
+
+//console.log(num);
+
+
+
+// *******************   const  **********************
+
+//const name; // 'const' declarations must be initialized.ts(1155)
+
+const name="valan";
+
+console.log(name);
