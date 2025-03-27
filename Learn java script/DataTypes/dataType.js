ac9993da58800fb883cd903358bce7f18cbb57af
pradeeb <valanpradeep6@gmail.com>
2025-03-27 19:06:52 +0530

    data type

---
+++ Learn java script/DataTypes/dataType.js
@@ -0,0 +1,44 @@
+//************************ primitive data
+
+
+//Number
+var age=15;
+var valie=15.5;
+
+//String
+let name1="valan";
+let name2='valan';
+let name3=`valan`;
+let name4="valan";
+var name="valan";
+var name='valan';
+var name=`valan`;
+
+//boolean
+let yes=true;
+
+//undefined
+let palce;  //undefined if did give value
+
+//null value
+let user=null;
+
+//************************ Non primitive data type OR referance datatype
+
+//Array
+let users=["valan","pradeeb"]
+//console.log(users);
+//console.log(users[1]);
+//console.log(users.length);
+
+
+//Objext
+let student={
+    id:1,
+    age:18,
+    place:"chennai"
+}
+
+//console.log(student.age);
+
+
