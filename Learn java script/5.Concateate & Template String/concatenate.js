3d5a28726c8c503aa4598e8c86709739d3bc1fcb
pradeeb <valanpradeep6@gmail.com>
2025-04-01 12:47:07 +0530

    concatenate littrel

---
+++ Learn java script/5.Concateate & Template String/concatenate.js
@@ -0,0 +1,9 @@
+let fn="valan ";
+let ln='pradeeb ';
+
+let full_name=fn+ln+"is a continuse learner";
+console.log(full_name);
+
+//Template string or template literal
+let literal=`${fn} ${ln} is new leaner`
+console.log(literal);
