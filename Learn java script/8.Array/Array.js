00e30f0999e358e04e793ea62f1dc8dd50bc7522
pradeeb <valanpradeep6@gmail.com>
2025-04-01 15:01:03 +0530

    Array Basicks

---
+++ Learn java script/8.Array/Array.js
@@ -0,0 +1,16 @@
+
+
+let flaver=["vanila","butterscortch","lavende",2];
+
+console.log(flaver.length);
+console.log(flaver[0]);
+console.log(flaver[2]);
+console.log(flaver[3]); //retuen undefined
+
+//Dense array
+
+let num=[1,2,3];   //continious idex memeory
+
+//sparse
+let num1=[1,,2,,5]; //hash table use store
+ 
