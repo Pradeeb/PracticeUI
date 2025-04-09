82396d106d9124bde8bd424c71d80fe522f1b97b
pradeeb <valanpradeep6@gmail.com>
2025-04-09 14:58:42 +0530

    feet:promise

---
+++ Learn java script/20.Promise/Promise.js
@@ -0,0 +1,49 @@
+
+
+/*
+let newPromise = new Promise((resolve, reject) => {
+    let response = true;
+
+    if (response) {
+        resolve("Success!");
+    } else {
+        reject("Failure!");
+    }
+});
+
+
+newPromise.then(
+    (success)=>{
+       console.log(success);
+    }
+).catch(
+    (fail)=>{
+        console.log(fail);
+    }
+    
+).finally(
+    ()=>{
+        console.log("end");
+    }
+);
+*/
+
+// create call back hell
+
+function fetchProfile(successFun, failFun) {
+    let is=true;
+
+    if(is){
+        successFun("sucess...");
+    }else{
+        failFun("fail")
+    }
+
+}
+
+fetchProfile(
+    (suncess)=>{console.log(suncess);
+        fetchProfile( (suncess)=>{console.log("Next "+suncess);}, (error)=>{console.log("Next "+error);});
+    },
+    (error)=>{console.log(error);}
+)
