6e3927227a402d88205c654ce5394746fc7117c2
Pradeeb <bg-dit@bhavishgraphics.com>
2025-06-17 12:49:22 +0530

    creat object insde array

--- Learn java script/9.Object/object.js
+++ Learn java script/9.Object/object.js
@@ -1,17 +1,32 @@
-let useProfile={
-    userName:"pradeeb",
-    age:30,
-    eyeColor:"black",
-    hairColor:"black",
-    eat: function (){
-       console.log("eating");
-       return "vanial icecream";
-    }
-}
-
+let useProfile = {
+  userName: "pradeeb",
+  age: 30,
+  eyeColor: "black",
+  hairColor: "black",
+  eat: function () {
+    console.log("eating");
+    return "vanilla ice cream";
+  },
+  favoriteFoods: [
+    { name: "biriyani", type: "rice", spicy: true },
+    { name: "dosa", type: "tiffin", spicy: false },
+    { name: "ice cream", type: "dessert", spicy: false }
+  ]
+};
 //console.log(useProfile.hairColor,useProfile.age,useProfile.eat());
+/*
+// Access full array
+console.log(useProfile.favoriteFoods);
 
+// Access 1st food name
+console.log(useProfile.favoriteFoods[0].name); // biriyani
 
+// Access 2nd food type
+console.log(useProfile.favoriteFoods[1].type); // tiffin
+
+// Access 3rd food spicy value
+console.log(useProfile.favoriteFoods[2].spicy); // false
+*/
 const vehicle ={
     type:"four wheeler",
     price:2000,
