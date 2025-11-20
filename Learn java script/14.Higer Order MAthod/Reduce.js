9ab63d939bba00ae740089bd0a9dd964f818ebc3
Pradeeb <valanpradeep6@gmail.com>
2025-11-20 12:29:11 +0530

    reduce

--- Learn java script/14.Higer Order MAthod/Reduce.js
+++ Learn java script/14.Higer Order MAthod/Reduce.js
@@ -1,7 +1,45 @@
+/*
+reduce() is a higher-order function available on arrays.
+It takes a callback function and reduces the array to a single value.
 
+----structure-------
 
-let num=[1,2,3];
+array.reduce((accumulator, currentValue) => {
+    // logic
+}, initialValue);
 
-let add=num.reduce((acc,eElement,index,arr)=>{ return acc+eElement},0)
+------Parameters----------
+Name	            Meaning
 
-console.log(add);
+accumulator	    Stores the result of previous operations
+currentValue	Current element in the array
+initialValue	(Optional) starting value of accumulator
+
+*/
+const arr = [1, 2, 3, 4];
+
+const sum = arr.reduce((acc, curr) => acc + curr, 0);
+
+console.log(sum); // 10
+
+//Ex 2
+const arr1 = [10, 50, 20, 80, 30];
+
+const max = arr1.reduce((acc, curr) => acc > curr ? acc : curr);
+
+console.log(max); // 80
+
+//Ex 3
+const users = [
+  { id: 1, name: "Pradeep" },
+  { id: 2, name: "Arun" }
+];
+
+const map = users.reduce((acc, user) => {
+  acc[user.id] = user.name;
+  return acc;
+}, {});
+
+console.log(map);
+// {1: "Pradeep", 2: "Arun"}
+
